78deb2751237c92c855d8cd01475699d0da6b45b
assehe marie claire <marieclaraassehe15@gmail.com>
2025-04-24 15:40:19 +0200

    Delete usermanager directory

--- usermanager/src/main/java/teck/marie/usermanagement/Repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package teck.marie.usermanagement.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import teck.marie.usermanagement.Entity.Users;
-
-public interface UserRepository extends JpaRepository<Users, Integer> {
-    Users findByEmail(String email);
-}
